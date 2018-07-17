7d727cbae5c1f772b7aeda7153a38cdb627937c5
peachykeen5 <lexi.keen@gmail.com>
2018-07-17 18:16:24 +0000

    completed exercises 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first
+@store2 = Store.find_by(name: "Richmond")
+
+@store1.name = "White Rock"
+@store1.save
+
+p @store2
+p Store.all
