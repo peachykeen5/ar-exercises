3b6b2ce5fdb6d2f00861b42971788c4b28d1c401
peachykeen5 <lexi.keen@gmail.com>
2018-07-17 19:07:53 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,17 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+
+@number_of_stores = Store.count
+
+@average_revenue = @total_revenue / @number_of_stores
+
+@high_revenue = Store.where("annual_revenue > 1000000").count
+
+puts @total_revenue
+puts @number_of_stores
+puts @average_revenue
+puts @high_revenue
+
+#Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
